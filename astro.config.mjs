430e3cd58d8138385405305116b38d665d6abae8
Maxence Sylvestre <maxence.sylvestre@msylv.com>
2026-06-01 13:49:47 +0200

    Liens page boursier, patch breadcrumbs pour les pages partenaires

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,5 +25,19 @@ export default defineConfig({
   },
   integrations: [icon(), mdx(), sitemap()],
   publicDir: 'static',
-  compressHTML: true
+  compressHTML: true,
+  redirects: {
+    '/formation': '/partenaires',
+    '/formation/ens-paris-saclay': '/partenaires/ens-paris-saclay',
+    '/formation/hec-paris': '/partenaires/hec-paris',
+    '/formation/institut-polytechnique-de-paris': '/partenaires/institut-polytechnique-de-paris',
+    '/formation/lycee-international-de-palaiseau': '/partenaires/lycee-international-de-palaiseau',
+    '/formation/universite-paris-saclay': '/partenaires/universite-paris-saclay',
+    '/en/formation': '/en/partenaires',
+    '/en/formation/ens-paris-saclay': '/en/partenaires/ens-paris-saclay',
+    '/en/formation/hec-paris': '/en/partenaires/hec-paris',
+    '/en/formation/institut-polytechnique-de-paris': '/en/partenaires/institut-polytechnique-de-paris',
+    '/en/formation/lycee-international-de-palaiseau': '/en/partenaires/lycee-international-de-palaiseau',
+    '/en/formation/universite-paris-saclay': '/en/partenaires/universite-paris-saclay',
+  }
 });
